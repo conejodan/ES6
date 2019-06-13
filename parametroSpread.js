9bf1465451abfb802ca0fb6d07e2988246b10975
DanielCornelio <jdcornelio89@gmail.com>
2019-06-13 16:47:14 -0500

    Promesas

--- parametroSpread.js
+++ parametroSpread.js
@@ -1,5 +1,5 @@
 const mostrarDatos = (...info)=>{
-    console.log(info)
+    console.log(info);
 }
 const arregloDatos=['jose', 29, 'correo@correo.com', 9931218256];
 mostrarDatos(...arregloDatos);
